922dfbef3e0b6a29c6de4e1938a224c57b30ca6b
mgermaine93 <mgermaine93@gmail.com>
2021-11-13 16:05:16 -0500

    Finished adding cities and towns to constants file.  Just need to clean it up a bit and remove duplicates, if possible.

--- metro_area_finder.py
+++ metro_area_finder.py
@@ -34,7 +34,7 @@ service = Service(ChromeDriverManager().install())
 PATH = "/Users/mgermaine93/Desktop/CODE/automatic-form-completion/chromedriver"
 
 # uses the link to the wikipedia page of a given town or city
-link = "https://en.wikipedia.org/wiki/Milwaukee_metropolitan_area"
+link = "https://en.wikipedia.org/wiki/Hidalgo_County,_Texas"
 
 # temporary function to be used to retrieve the surrounding towns and cities of a given larger city
 
