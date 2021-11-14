8b94611f97e86bb7fbddfb81db054b25daf8fce4
mgermaine93 <mgermaine93@gmail.com>
2021-11-14 15:19:07 -0500

    Finished cleaning up cities and towns data.

---
+++ constants/idaho.py
@@ -0,0 +1,18 @@
+IDAHO_POCATELLO = [
+    "Arimo, ID",
+    "Chubbuck, ID",
+    "Downey, ID",
+    "Inkom, ID",
+    "Lava Hot Springs, ID",
+    "McCammon, ID",
+    "Pocatello, ID",
+    "Fort Hall, ID",
+    "Tyhee, ID",
+    "Blackrock, ID",
+    "Portneuf, ID",
+    "Red Rock Junction, ID",
+    "Robin, ID",
+    "Swanlake, ID",
+    "Virginia, ID",
+    "Zenda, ID"
+]
---
+++ constants/kentucky.py
@@ -0,0 +1,14 @@
+KENTUCKY_SPRINGFIELD = [
+    "Bear Wallow, KY",
+    "Brush Grove, KY",
+    "Fredericktown, KY",
+    "Mackville, KY",
+    "Manton, KY",
+    "Pleasant Grove, KY",
+    "Saint Catharine, KY",
+    "Springfield, KY",
+    "Thompsonville, KY",
+    "Willisburg, KY",
+    "Mooresville, KY",
+    "Maud, KY"
+]
